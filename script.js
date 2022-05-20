72eb8b5c843091ef47a10b20812168f52f45ebd4
Dana Alami <dana.alami18@gmail.com>
2022-05-20 13:57:33 +0300

    fixed the issue

--- script.js
+++ script.js
@@ -1,46 +1,45 @@
-const addItems = document.querySelector(".add-items");
-const itemsList = document.querySelector(".plates");
-const items = JSON.parse(localStorage.getItem("items")) || [];
+const addItems = document.querySelector('.add-items');
+  const itemsList = document.querySelector('.plates');
+  const items = JSON.parse(localStorage.getItem('items')) || [];
 
-function addItem(e) {
-  e.preventDefault(); // stops the page from reloading
-  const text = this.querySelector("[name=item]").value;
-  const item = {
-    text,
-    done: false,
-  };
-  items.push(item);
-  populateList(items, itemsList);
-  localStorage.setItem('items', JSON.stringify(items));
-  this.reset(); //"this" is the form element
-}
+  function addItem(e) {
+    e.preventDefault();
+    const text = (this.querySelector('[name=item]')).value;
+    const item = {
+      text,
+      done: false
+    };
 
-function populateList(plates = [], platesList) {
-  platesList.innerHTML = plates
-    .map((plate, i) => {
+    items.push(item);
+    populateList(items, itemsList);
+    localStorage.setItem('items', JSON.stringify(items));
+    this.reset();
+  }
+
+  function populateList(plates = [], platesList) {
+    platesList.innerHTML = plates.map((plate, i) => {
       return `
         <li>
-        <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : '' } />
-        <label for="item${i}">${plate.text}</label>
+          <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
+          <label for="item${i}">${plate.text}</label>
         </li>
-        `;
+      `;
     }).join('');
-}
+  }
 
-function toggleDone(e) {
-  if (!e.target.matches('input')) return; //skip this unless its an input
-  const el = e.target;
-  const index = el.dataset.index;
-  items[index].done = !items[index].done;
-  localStorage.setItem('items', JSON.stringify(items));
+  function toggleDone(e) {
+    if (!e.target.matches('input')) return; // skip this unless it's an input
+    const el = e.target;
+    const index = el.dataset.index;
+    items[index].done = !items[index].done;
+    localStorage.setItem('items', JSON.stringify(items));
+    populateList(items, itemsList);
+  }
+
+  addItems.addEventListener('submit', addItem);
+  itemsList.addEventListener('click', toggleDone);
+
   populateList(items, itemsList);
-}
-
-addItems.addEventListener('submit', addItem);
-
-itemsList.addEventListener('click', toggleDone);
-
-populateList(items, itemsList);
 
 // localstorage is objects stored in the browser. save texts to the browser
 //localstorage only takes string
